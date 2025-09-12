32f42937ce9c3c02254564308b1f603f253725d4
juannpabbllo <159568126+juannpabbllo@users.noreply.github.com>
2025-09-12 16:26:14 -0600

    practica 5

---
+++ 005_Matematicas_Basicas.py
@@ -0,0 +1,36 @@
+operacion_1 = 54+78 # Suma
+print(operacion_1)
+operacion_2 = 54-78 # Resta
+print(operacion_2)
+operacion_3 = 54*78 # Multiplicación
+print(operacion_3)
+operacion_4= 54/78 # División
+print(operacion_4)
+operacion_5 = 54//78 # División entera
+print(operacion_5)
+operacion_6 = 54%78 # Módulo
+print(operacion_6)
+operacion_7 = 54**2 # Potencia
+print(operacion_7)
+operacion_8 = 54**0.5 # Raíz cuadrada
+print(operacion_8)
+operacion_9 = 54**(1/3) # Raíz cúbica
+print(operacion_9)
+operacion_10 = 54**3 # Potencia cúbica
+print(operacion_10)
+operacion_11 = 54+78*2 # Prioridad de operaciones
+print(operacion_11)
+operacion_12 = (54+78)*2 # Uso de paréntesis para cambiar la prioridad de operaciones
+print(operacion_12)
+operacion_13 = 54>78 # Mayor que
+print(operacion_13)
+operacion_14 = 54<78 # Menor que
+print(operacion_14)
+operacion_15 = 54>=78 # Mayor o igual que
+print(operacion_15)
+operacion_16 = 54<=78 # Menor o igual que
+print(operacion_16)
+operacion_17 = 54==78 # Igual que
+print(operacion_17)
+operacion_18 = 54!=78 # Diferente que
+print(operacion_18)
