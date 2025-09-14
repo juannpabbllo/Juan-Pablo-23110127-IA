b75fe6af0bf08f35b79a5a1cca931d40d2f617a4
juannpabbllo <159568126+juannpabbllo@users.noreply.github.com>
2025-09-14 17:23:24 -0600

    practica 8

---
+++ 008_Listas_y_Arreglos.py
@@ -0,0 +1,4 @@
+#Lista de Python
+camiseta=["rojo", "L", 100, 10] # Lista que contiene varios tipos de datos
+print(camiseta) # Imprime la lista completa
+print(camiseta[0]) # Imprime el primer elemento de la lista (color)
